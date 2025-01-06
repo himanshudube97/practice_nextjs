33dcc174ad95a23646018548886e93102537d409
himanshudube97 <himanshu.dube13@gmail.com>
2025-01-07 00:11:20 +0530

    react_project test

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Home } from "./Home/Home";
+
+
+export const App = () => {
+    return (
+        <div>
+        <h1 className={"bg-red-600"}>App</h1>
+        <Home/>
+        </div>
+    );
+    }
